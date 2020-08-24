aea0a40030558c1d3ca88db9e6a0050deb414fd6
ramza007 <ramzalvo007@gmail.com>
2020-08-24 19:32:24 -0400

    - Added new 'Tech' field to Project model - Adjusted Project template styling to accomodate more verbiage

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Project(models.Model):
     image = models.ImageField(upload_to='photos/site_images', null=True)
     name = models.CharField(max_length=10)
+    tech = models.CharField(max_length=20, null=True)
     projLink = models.URLField(max_length=128, db_index=True, unique=True, blank=False)
     repoLink = models.URLField(max_length=128, unique=True, blank=False)
 
